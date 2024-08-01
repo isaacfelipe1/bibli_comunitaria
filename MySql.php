447c947215ca31031b11b72cfdd057e7cbb228b4
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-07-31 22:04:11 -0300

    feat: addhost in setup

--- MySql.php
+++ MySql.php
@@ -7,7 +7,7 @@
 		
 		public static function connect(){
 			if(!isset($pdo)){
-				$pdo = new PDO('mysql:host=localhost;dbname=u397599254_bibli','u397599254_root','Bibli123456#');
+				$pdo = new PDO('mysql:host=srv1182.hstgr.io;dbname=u397599254_bibli','u397599254_root','Bibli123456#');
 			}
 
 			return $pdo;
