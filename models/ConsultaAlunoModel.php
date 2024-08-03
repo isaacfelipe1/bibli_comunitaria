67e95abce0aa82a7679f53121911a6fc81739aa2
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-08-02 23:58:29 -0300

    feat: remove commntes

--- models/ConsultaAlunoModel.php
+++ models/ConsultaAlunoModel.php
@@ -3,8 +3,6 @@
 	date_default_timezone_set('America/Sao_Paulo');
 	class ConsultaAlunoModel extends Model
 	{
-
-		//Aqui é o método para puxar os clientes!
 		public static function listarAlunos(){
 		$usuarios = \MySql::connect()->prepare("SELECT * FROM alunos order by nome");
 		$usuarios->execute();
