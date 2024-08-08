35012e6fc0806988dbf8983e147798d5e9e1752d
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-08-08 03:18:59 -0300

    fiX: add _blank

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -5,13 +5,13 @@
             <hr>
         </div>
         <div class="contato">
-            <a href="https://m.facebook.com/p/Biblioteca-Comunitária-Maria-Dolores-100078250148289/"><i class="bi bi-facebook"></i></a>
-            <a href="#"><i class="bi bi-envelope-at-fill"></i></a>
-            <a href="https://www.instagram.com/bibliotecacomunitariamd/"><i class="bi bi-instagram"></i></a>
-            <a href="https://l.instagram.com/?u=http%3A%2F%2Fwa.me%2F5592994662163%2F&e=AT09yciPZIR3H7-kpwpJDJX7267bE8tZnYt28R5sNab5Mx3L9acsgjo_kNu_Gs6pepoR2ZA-2JKIVbr2KjaS778wpDIY34XX"><i class="bi bi-whatsapp"></i></a>
+            <a href="https://m.facebook.com/p/Biblioteca-Comunitária-Maria-Dolores-100078250148289/" target="_blank"><i class="bi bi-facebook"></i></a>
+            <a href="#" target="_blank"><i class="bi bi-envelope-at-fill"></i></a>
+            <a href="https://www.instagram.com/bibliotecacomunitariamd/" target="_blank"><i class="bi bi-instagram"></i></a>
+            <a href="https://l.instagram.com/?u=http%3A%2F%2Fwa.me%2F5592994662163%2F&e=AT09yciPZIR3H7-kpwpJDJX7267bE8tZnYt28R5sNab5Mx3L9acsgjo_kNu_Gs6pepoR2ZA-2JKIVbr2KjaS778wpDIY34XX" target="_blank"><i class="bi bi-whatsapp"></i></a>
         </div>
         <div id="outras-informacoes" class="outras-informacoes">
-            <p><a href="https://goo.gl/maps/t4yDenK7SBRo2d5P9">Rua da Felicidade, N°3725 Jardim Amanda, Itacoatiara, AM, Brazil</a></p>
+            <p><a href="https://goo.gl/maps/t4yDenK7SBRo2d5P9" target="_blank">Rua da Felicidade, N°3725 Jardim Amanda, Itacoatiara, AM, Brazil</a></p>
             <p><a href="mailto:bcmd@gmail.com">bcmd@gmail.com</a></p>
             <p>Desenvolvido pelo projeto de extensão &reg;</p>
         </div>
