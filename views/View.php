e1aa2b375f3f98be7b227b4c150ea8b853bc00f3
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-08-08 03:20:14 -0300

    styles: remove commst

--- views/View.php
+++ views/View.php
@@ -7,14 +7,13 @@ class View {
     const DEFAULT_FOOTER = 'footer.php';
 
     public function render($body, $header = null, $footer = null) {
-        // Inclui o header padrão se um personalizado não for fornecido
         if ($header == null) {
             include('views/templates/' . self::DEFAULT_HEADER);
         } else {
             include('views/templates/' . $header);
         }
         include('views/templates/' . $body);
-		
+
         if ($footer == null) {
             include('views/templates/' . self::DEFAULT_FOOTER);
         } else {
