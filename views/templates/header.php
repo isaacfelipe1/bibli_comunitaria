cf7a1ef7377f370b3922070b3353cb506f8bee64
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-08-08 04:45:36 -0300

    feat: add past navbar

--- views/templates/header.php
+++ views/templates/header.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="views/templates/css/style_default.css">
     <link rel="stylesheet" href="views/templates/js/script.js">
     <link rel="stylesheet" href="views/templates/css/footer.css"> 
+    <link rel="stylesheet" href="views/templates/css/navbar.css">
     <link rel="icon" href="views/templates/img/favicon-16x16.png">
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
@@ -27,7 +28,7 @@
           </div>
         </div>
       </nav> 
-            <span class="square"></span>
+            <!-- <span class="square"></span> -->
             <h1> Biblioteca Comunitária<br> Maria Dolores</h1>
         </div>
     </header>
