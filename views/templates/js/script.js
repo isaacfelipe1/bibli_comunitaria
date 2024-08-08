90dc9bf6c3a357c6af321c3de801903fc4227688
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-08-08 02:09:05 -0300

    feat: remove coments

--- views/templates/js/script.js
+++ views/templates/js/script.js
@@ -1,5 +1,5 @@
 function formatPhoneNumber(input) {
-    const value = input.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+    const value = input.value.replace(/\D/g, ''); 
     if (value.length <= 10) {
       input.value = value.replace(/^(\d{2})(\d{4})(\d{4})$/, '($1) $2-$3');
     } else {
