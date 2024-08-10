b6a62821f3780a032b3470623c9f6f2fe46b532f
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-08-10 01:24:09 -0300

    feat: add card user

--- views/templates/menuUsuario.php
+++ views/templates/menuUsuario.php
@@ -6,47 +6,60 @@ if (!isset($_SESSION['user'])) {
 }
 ?>
 <div id="div-cadastro">
-        <div id="div-opcao-cadastro">
-            <a href="cadastroUsuario">
+    <div id="div-opcao-cadastro">
+        <a href="cadastroUsuario">
             <center><i class="bi bi-person" style="color:black; font-size: 2.5rem;"></i></center>
-            <a id="a-opcao-cadastro" href="cadastroUsuario">Cadastro</a></a>
-        </div>
-        
-        <div id="div-opcao-cadastro">
-            <a href="cadastroAluno">
+            <a id="a-opcao-cadastro" href="cadastroUsuario">Cadastro</a>
+        </a>
+    </div>
+    
+    <div id="div-opcao-cadastro">
+        <a href="cadastroAluno">
             <center><i class="bi bi-person-check" style="color:black; font-size: 2.5rem;"></i></center>
-            <a id="a-opcao-cadastro" href="cadastroAluno">Matricula</a></a>
-        </div>
-        
-        <div id="div-opcao-cadastro">
-            <a href="consultaAluno">
+            <a id="a-opcao-cadastro" href="cadastroAluno">Matricula</a>
+        </a>
+    </div>
+    
+    <div id="div-opcao-cadastro">
+        <a href="consultaAluno">
             <center><i class="bi bi-person-check" style="color:black; font-size: 2.5rem;"></i></center>
-            <a id="a-opcao-cadastro" href="consultaAluno">Consulta Matricula</a></a>
-        </div>
+            <a id="a-opcao-cadastro" href="consultaAluno">Consulta Matricula</a>
+        </a>
+    </div>
 
-        <div id="div-opcao-cadastro">
-            <a href="consultaUsuario">
+    <div id="div-opcao-cadastro">
+        <a href="consultaUsuario">
             <center><i class="bi bi-people" style="color:black; font-size: 2.5rem;"></i></center>
-            <a id="a-opcao-cadastro" href="consultaUsuario" >Todos os usuários</a>
-            </a>
-        </div>
+            <a id="a-opcao-cadastro" href="consultaUsuario">Todos os usuários</a>
+        </a>
+    </div>
 
-        <div id="div-opcao-cadastro">
-            <a href="selecaoExclusaoUsuario">
+    <div id="div-opcao-cadastro">
+        <a href="selecaoExclusaoUsuario">
             <center><i class="bi bi-trash3" style="color:black; font-size: 2.5rem;"></i></center>
-            <a id="a-opcao-cadastro" href="selecaoExclusaoUsuario">Excluir</a></a> 
-        </div>
-        
-
-        <div id="div-opcao-cadastro">
-            <a href="selecaoAlterarUsuario">
+            <a id="a-opcao-cadastro" href="selecaoExclusaoUsuario">Excluir</a>
+        </a> 
+    </div>
+    
+    <div id="div-opcao-cadastro">
+        <a href="selecaoAlterarUsuario">
             <center><i class="bi bi-pen" style="color:black; font-size: 2.5rem;"></i></center>
-            <a id="a-opcao-cadastro" href="selecaoAlterarUsuario">Alterar Informações</a></a>
-        </div>
+            <a id="a-opcao-cadastro" href="selecaoAlterarUsuario">Alterar Informações</a>
+        </a>
+    </div>
 
-        <div id="div-opcao-cadastro" style="display:inline-block">
-            <a href="formPesquisaUsuario">
+    <div id="div-opcao-cadastro" style="display:inline-block">
+        <a href="formPesquisaUsuario">
             <center><i class="bi bi-search" style="color:black; font-size: 2.5rem"></i></center>
-            <a id="a-opcao-cadastro" href="formPesquisaUsuario">Pesquisar Usuário</a></a>
-        </div>
+            <a id="a-opcao-cadastro" href="formPesquisaUsuario">Pesquisar Usuário</a>
+        </a>
+    </div>
+
+    <!-- Novo card para cadastro -->
+    <div id="div-opcao-cadastro" style="display:inline-block; margin-top: 1rem;">
+        <a href="cadastroLogin">
+            <center><i class="bi bi-person-plus" style="color:black; font-size: 2.5rem;"></i></center>
+            <a id="a-opcao-cadastro" href="cadastroLogin">Cadastrar usuário</a>
+        </a>
+    </div>
 </div>
