dea54279d6428861a16c973b0a0452d59c581870
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-08-11 05:01:58 -0300

    feat: reajust table

--- views/templates/consultaUsuario.php
+++ views/templates/consultaUsuario.php
@@ -1,38 +1,36 @@
 <main>
     <h2>Todos os usuários</h2>
-    <table id="customers">
-    <tr>
-            <th>Codigo</th>
-            <th>Nome</th>
-            <th>Sexo</th>
-            <th>Idade</th>
-            <th>Telefone</th>
-            <th>Endereco</th>
-            <th>Observacao</th>
-    </tr>
-        <?php 
-            $livros = models\ConsultaUsuarioModel::listarUsuarios();
-            foreach ($livros as $value) {
-        ?>
-            <tr>
-                <td><?php echo $value['id_usuario']?></td>
-                <td><?php echo $value['nome']?></td>
-                <td><?php 
-                $sexo = $value['sexo'];
-                if ($sexo == 1) {
-                    echo 'Masculino';
-                } else {
-                    echo 'Feminino';
-                } 
-                // echo $value['sexo']?></td>
-                <td><?php echo $value['idade']?></td>
-                <td><?php echo $value['telefone']?></td>
-                <td><?php echo $value['endereco']?></td>
-                <td><?php echo $value['observacao']?></td>
-            </tr>
-
-        <?php 
-            }
-        ?>
-    </table>
+    <div class="table-container">
+        <table id="customers">
+            <thead>
+                <tr>
+                    <th>Código</th>
+                    <th>Nome</th>
+                    <th>Sexo</th>
+                    <th>Idade</th>
+                    <th>Telefone</th>
+                    <th>Endereço</th>
+                    <th>Observação</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    $usuarios = models\ConsultaUsuarioModel::listarUsuarios();
+                    foreach ($usuarios as $value) {
+                ?>
+                <tr>
+                    <td><?php echo $value['id_usuario']?></td>
+                    <td><?php echo $value['nome']?></td>
+                    <td><?php echo ($value['sexo'] == 1) ? 'Masculino' : 'Feminino'; ?></td>
+                    <td><?php echo $value['idade']?></td>
+                    <td><?php echo $value['telefone']?></td>
+                    <td><?php echo $value['endereco']?></td>
+                    <td><?php echo $value['observacao']?></td>
+                </tr>
+                <?php 
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </main>
