bde60a3157ba77257ac6f07546e9c8b12be2f7d2
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-08-11 04:51:24 -0300

    feat: add table readjustment

--- views/templates/selecaoFichas.php
+++ views/templates/selecaoFichas.php
@@ -9,38 +9,44 @@ if (!isset($_SESSION['user'])) {
 <main>
     <h2>Selecione os campos para criar fichas</h2>
     
-
     <?php
     use models\SelecaoFichasModel;
     $excl = new SelecaoFichasModel();
     $excl->limparFichas();
     ?>
-<form method="post">
-<a style="background-color: #04aa6d;text-decoration:none" class="ficha-codigo" href="fichas">Ver Fichas</a>
-<input class="ficha-codigo" type="submit" name='bt_limp_fich' value="Limpar fichas">
+    <form method="post">
+        <a class="ficha-codigo" href="fichas" style="background-color: #047f52">Ver Fichas</a> <!-- Botão verde -->
+        <input class="ficha-codigo" type="submit" name='bt_limp_fich' value="Limpar fichas">
 
-<table id="customers" style="border: 1px solid black;">
-<tr>
-        <th>#</th>
-        <th>Codigo</th>
-        <th>Titulo</th>
-</tr>
-<?php 
-    $livros = models\ConsultaLivroModel::listarLivros();
-    foreach ($livros as $value) {
-?>
-    <tr>
-        <td><input type="checkbox" name="cod_livro[]" value="<?php echo $value['cod_livro']?>"></td>
-        <td><?php echo $value['cod_livro']?></td>
-        <td><?php echo $value['letra_titulo']?></td>
-    </tr>
+        <div class="table-container">
+            <table id="customers">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Código</th>
+                        <th>Título</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                    $livros = models\ConsultaLivroModel::listarLivros();
+                    foreach ($livros as $value) {
+                    ?>
+                    <tr>
+                        <td><input type="checkbox" name="cod_livro[]" value="<?php echo $value['cod_livro']?>"></td>
+                        <td><?php echo $value['cod_livro']?></td>
+                        <td><?php echo $value['letra_titulo']?></td>
+                    </tr>
+                    <?php 
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
 
-<?php 
-    }
-?>
-</table>
-    <input style="margin-left: 2rem;" class='bt-submit' type='submit' name='bt_fich' value='Criar'>
-</form>
+        <input class='bt-submit mt-3' type='submit' name='bt_fich' value='Criar'>
+
+    </form>
 </main>
 
 <?php
