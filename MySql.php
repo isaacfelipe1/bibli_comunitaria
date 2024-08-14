52ccd5e4fe378ee3141b45cc1fef3a704bde2b80
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-08-14 04:34:04 -0300

    style: remove space

--- MySql.php
+++ MySql.php
@@ -5,9 +5,8 @@
 	
 		public static function connect(){
 			if(!isset(self::$pdo)){
-				// Substitua com as credenciais do seu banco de dados local
-				self::$pdo = new PDO('mysql:host=localhost;dbname=u397599254_bibli','root',''); // usuário 'root' e senha vazia no WampServer por padrão
-				// Opcionalmente, você pode definir o modo de erro para excepcionar:
+				
+				self::$pdo = new PDO('mysql:host=localhost;dbname=u397599254_bibli','root',''); 
 				self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			}
 	
