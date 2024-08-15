092cb574c0d3fa6b93af95bf8a6887898d4a55f4
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-08-15 12:29:02 -0300

    feat: remove add photo in login

--- models/CadastroLoginModel.php
+++ models/CadastroLoginModel.php
@@ -1,5 +1,5 @@
 <?php
-	namespace models;
+    namespace models;
 
     class CadastroLoginModel extends Model
     {
@@ -8,43 +8,28 @@
             if (isset($_POST["bt_enviar"])) {
                 $nome_usuario = $_POST["nome_usuario"];
                 $senha = $_POST["senha"];
-                $foto = $_FILES['foto'];
     
                 $password_hash = password_hash($senha, PASSWORD_DEFAULT);
 
-                $fotoNome = uniqid() . '-' . $foto['name']; 
-                $fotoDiretorio = 'uploads/' . $fotoNome; 
+                $cadastro = \MySql::connect()->prepare("INSERT INTO login (username, password_hash) VALUES (?, ?)");
+                $cadastro->execute([$nome_usuario, $password_hash]);
     
-                if (!file_exists('uploads')) {
-                    mkdir('uploads', 0777, true);
-                }
-    
-                if (move_uploaded_file($foto['tmp_name'], $fotoDiretorio)) {
-                    $cadastro = \MySql::connect()->prepare("INSERT INTO login (username, password_hash, foto) VALUES (?, ?, ?)");
-                    $cadastro->execute([$nome_usuario, $password_hash, $fotoNome]);
-    
-                    if ($cadastro->rowCount() > 0) {
-                        echo "<script>
-                                function cadastro(){
-                                    alert('Cadastro realizado com sucesso!')
-                                }
-                                cadastro();
-                              </script>";
-                    } else {
-                        echo "<script>
-                                function cadastro(){
-                                    alert('Erro durante o cadastro!')
-                                }
-                                cadastro();
-                              </script>";
-                    }
+                if ($cadastro->rowCount() > 0) {
+                    echo "<script>
+                            function cadastro(){
+                                alert('Cadastro realizado com sucesso!')
+                            }
+                            cadastro();
+                          </script>";
                 } else {
                     echo "<script>
-                            alert('Erro ao enviar a foto.');
+                            function cadastro(){
+                                alert('Erro durante o cadastro!')
+                            }
+                            cadastro();
                           </script>";
                 }
             }
         }
     }
-    
 ?>
