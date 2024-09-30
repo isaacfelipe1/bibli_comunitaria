22ed59df790b5f8e17415e8ddf9df89b4ed52e3e
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-09-30 00:04:58 -0300

    feat: reajuste do icone de voltar

--- views/templates/menu.php
+++ views/templates/menu.php
@@ -9,56 +9,70 @@ if (!isset($_SESSION['user'])) {
 }
 ?>
 
-<form method="post" action="">
-    <p style="margin-top: 20px; margin-left: 20px">
-        <button class="bt-logout" type="submit" name="logout">
-            <i style="color:#047f52; font-size: 2.5rem" class="bi bi-box-arrow-left"></i>
-        </button>
-        <button class="bt-back" type="button" onclick="window.location.href='home'">
-            <i style="color:#047f52; font-size: 2.5rem" class="bi bi-arrow-left-circle"></i>
-        </button>
-    </p>
+<form method="post" action="" style="display: flex; align-items: center; margin: 20px;">
+    <button class="bt-back" type="button" onclick="window.location.href='home'" style="display: flex; align-items: center; background: none; border: none; cursor: pointer;">
+        <i style="color:#047f52; font-size: 2.5rem; margin-right: 10px;" class="bi bi-arrow-left-circle"></i>
+        <span style="font-size: 1.2rem; color: #047f52;"></span>
+    </button>
+    <button class="bt-logout" type="submit" name="logout" style="background: none; border: none; cursor: pointer; margin-left: auto;">
+        <i style="color:#047f52; font-size: 2.5rem;" class="bi bi-box-arrow-left"></i>
+    </button>
 </form>
-
 <div id="div-cadastro">
     <div id="div-opcao-cadastro">
-        <a href="cadastroLivro"><center><i class="bi bi-plus-lg" style="color:black; font-size: 2.5rem"></i></center>
-        <a id="a-opcao-cadastro" href="cadastroLivro">Cadastro</a>
+        <a href="cadastroLivro">
+            <center><i class="bi bi-plus-lg" style="color:black; font-size: 2.5rem"></i></center>
+            <a id="a-opcao-cadastro" href="cadastroLivro">Cadastro</a>
+        </a>
     </div>
 
     <div id="div-opcao-cadastro">
-        <a href="emprestimo"><center><i class="bi bi-book" style="color:black; font-size: 2.5rem"></i></center>
-        <a id="a-opcao-cadastro" href="emprestimo">Emprestimo</a>
+        <a href="emprestimo">
+            <center><i class="bi bi-book" style="color:black; font-size: 2.5rem"></i></center>
+            <a id="a-opcao-cadastro" href="emprestimo">Empréstimo</a>
+        </a>
     </div>
 
     <div id="div-opcao-cadastro">
-        <a href="consultaLivro"><center><i class="bi bi-list-columns" style="color:black; font-size: 2.5rem"></i></center>
-        <a id="a-opcao-cadastro" href="consultaLivro">Consulta</a>
+        <a href="consultaLivro">
+            <center><i class="bi bi-list-columns" style="color:black; font-size: 2.5rem"></i></center>
+            <a id="a-opcao-cadastro" href="consultaLivro">Consulta</a>
+        </a>
     </div>
 
     <div id="div-opcao-cadastro">
-        <a href="selecaoExclusaoLivro"><center><i class="bi bi-trash3" style="color:black; font-size: 2.5rem"></i></center>
-        <a id="a-opcao-cadastro" href="selecaoExclusaoLivro">Excluir</a>
+        <a href="selecaoExclusaoLivro">
+            <center><i class="bi bi-trash3" style="color:black; font-size: 2.5rem"></i></center>
+            <a id="a-opcao-cadastro" href="selecaoExclusaoLivro">Excluir</a>
+        </a>
     </div>
 
     <div id="div-opcao-cadastro">
-        <a href="selecaoAlterarLivro"><center><i class="bi bi-pen" style="color:black; font-size: 2.5rem"></i></center>
-        <a id="a-opcao-cadastro" href="selecaoAlterarLivro">Alterar Informações</a>
+        <a href="selecaoAlterarLivro">
+            <center><i class="bi bi-pen" style="color:black; font-size: 2.5rem"></i></center>
+            <a id="a-opcao-cadastro" href="selecaoAlterarLivro">Alterar Informações</a>
+        </a>
     </div>
 
     <div id="div-opcao-cadastro">
-        <a href="selecaoFichas"><center><i style="color:black; font-size: 2.5rem" class="bi bi-card-list"></i></center>
-        <a id="a-opcao-cadastro" href="selecaoFichas">Fichas</a>
+        <a href="selecaoFichas">
+            <center><i style="color:black; font-size: 2.5rem" class="bi bi-card-list"></i></center>
+            <a id="a-opcao-cadastro" href="selecaoFichas">Fichas</a>
+        </a>
     </div>
 
     <div id="div-opcao-cadastro">
-        <a href="selecaoDevolucao"><center><i class="bi bi-journal-arrow-down" style="color:black; font-size: 2.5rem"></i></center>
-        <a id="a-opcao-cadastro" href="selecaoDevolucao">Devolucao</a>
+        <a href="selecaoDevolucao">
+            <center><i class="bi bi-journal-arrow-down" style="color:black; font-size: 2.5rem"></i></center>
+            <a id="a-opcao-cadastro" href="selecaoDevolucao">Devolução</a>
+        </a>
     </div>
 
     <div id="div-opcao-cadastro">
-        <a href="selecaoCodigos"><center><i style="color:black; font-size: 2.5rem" class="bi bi-regex"></i></center>
-        <a id="a-opcao-cadastro" href="selecaoCodigos">Codigos</a>
+        <a href="selecaoCodigos">
+            <center><i style="color:black; font-size: 2.5rem" class="bi bi-regex"></i></center>
+            <a id="a-opcao-cadastro" href="selecaoCodigos">Códigos</a>
+        </a>
     </div>
 </div>
 
