b3bfc0ba80626bc3cbf784b1800db4188a39d67d
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-09-29 23:08:42 -0300

    fix; reajustando

--- models/AlterarEditarUsuarioModel.php
+++ models/AlterarEditarUsuarioModel.php
@@ -57,7 +57,7 @@ class AlterarEditarUsuarioModel extends Model
                         alert('Usuário alterado com sucesso!');
                     }
                     cadastro();
-                     window.location.href = window.location.origin + '/selecaoAlterarUsuario.php';
+                    window.location.href='localhost:8080/selecaoAlterarUsuario.php';
                 </script>";
             }
         }
